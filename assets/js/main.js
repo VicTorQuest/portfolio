adb4fa34971fad53742163da1604b470603742b0
Victor <victorokolie001@gmail.com>
2026-07-24 18:02:30 +0100

    added blogs and blog posta

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,28 +237,28 @@ on('click', '.cta-buttons a', function(e) {
 
      $("body").tooltip({ selector: '[data-toggle=tooltip]' });
 
-    // $.ajax({
-    //     type: 'GET',
-    //     url: '/get-blog-posts/',
-    //     success: function(response) {
-    //         setTimeout(function(){
-    //             $('#blogLoader').fadeOut()
-    //             for (var post in response.all_posts) {
-    //                 $('#posts').append(`<a href='${response.all_posts[post].url}' target='_blank' class='post-links'><div class='card--article-preview'><div class='article-preview__body'><h3 style='line-height: 0;'>📝</h3><div class='post'><strong>${response.all_posts[post].title}</strong><div class="mb-0" style="line-height:normal"><small>${new Date(response.all_posts[post].created_at).toLocaleDateString('en-US')}</small></div></div></div><i class='bi bi-arrow-right'></i></div></a>`)
-    //             }
-    //         }, 5000)
+    $.ajax({
+        type: 'GET',
+        url: 'https://questcoding.com/get-blog-posts/',
+        success: function(response) {
+            setTimeout(function(){
+                $('#blogLoader').fadeOut()
+                for (var post in response.all_posts) {
+                    $('#posts').append(`<a href='${response.all_posts[post].url}' target='_blank' class='post-links'><div class='card--article-preview'><div class='article-preview__body'><h3 style='line-height: 0;'>📝</h3><div class='post'><strong>${response.all_posts[post].title}</strong><div class="mb-0" style="line-height:normal"><small>${new Date(response.all_posts[post].created_at).toLocaleDateString('en-US')}</small></div></div></div><i class='bi bi-arrow-right'></i></div></a>`)
+                }
+            }, 5000)
             
             
             
-    //     },
-    //     error: function(response) {
-    //         setTimeout(() => {
-    //             $('#blogLoader').fadeOut()
-    //             $('#posts').append(`<p>Failed to load blog posts</p>`)
-    //         }, 5000);
+        },
+        error: function(response) {
+            setTimeout(() => {
+                $('#blogLoader').fadeOut()
+                $('#posts').append(`<p>Failed to load blog posts</p>`)
+            }, 5000);
            
-    //     }
-    // })
+        }
+    })
 
   });
 
